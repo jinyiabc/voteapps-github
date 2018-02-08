04c1a36d66cd5880d2a904b85ea91e384e911f88
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2018-02-08 18:42:11 +0800

    make it better!

--- app.js
+++ app.js
@@ -78,26 +78,26 @@ app.get('/profile',
 
 
 // initialize the routes
-app.use('/api',ensureAuthenticated,api);
+app.use('/api',api);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-//
-// // error handler
-// app.use(function(err, req, res, next) {
-//   console.log(err.message);
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  console.log(err.message);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
 
--- routes/api.js
+++ routes/api.js
@@ -3,32 +3,50 @@ const router = express.Router();
 const User = require('../models/users');
 
 // Get a list of polls from the db
-router.get('/polls',function(req,res,next){
-  res.send({type:'GET'})
+router.get('/:userId/polls',function(req,res,next){
+  res.send({userId:req.params.userId});
 });
 
 
 // Add a new poll to the db
-router.post('/polls',function(req,res,next){
-  Poll.create(req.body).then(function(poll){
-    res.send(poll)
+router.post('/:userId/polls',function(req,res,next){
+  const query = {'github.username':req.params.userId};
+//   req.body = {
+// 	              "title":"Thor or Camption America?",
+// 	              "options":["Thor","Camption America","Iron Man"]
+//              }
+  const update = {polls: {
+                        $each:[ req.body ],
+                        $sort: { score: -1 }
+                      }
+                 };
+  User.findOneAndUpdate(query,{$push:update},{upsert: true}).then(function(){   //upsert: bool - creates the object if it doesn't exist. defaults to false.
+
+    User.findOne(query).then(function(poll){
+      res.send(poll);
+    })
   }).catch(next);
 
 });
 
 // Update a poll in the db
-router.put('/polls/:id',function(req,res,next){
+router.put('/:userId/polls/:id',function(req,res,next){
   const query = {_id:req.params.id};
-  Poll.findOneAndUpdate(query,req.body,{upsert: true}).then(function(){   //upsert: bool - creates the object if it doesn't exist. defaults to false.
+  const update = {polls: {
+                        $each:[ req.body ],
+                        $sort: { score: -1 }
+                      }
+                 };
+  User.findOneAndUpdate(query,update,{upsert: true}).then(function(){   //upsert: bool - creates the object if it doesn't exist. defaults to false.
 
-    Poll.findOne(query).then(function(poll){
+    User.findOne(query).then(function(poll){
       res.send(poll);
     })
-  })
+  }).catch(next);
 });
 
 // Delete a poll from the db
-router.delete('/polls/:id',function(req,res,next){
+router.delete('/:userId/polls/:id',function(req,res,next){
   const query = {_id:req.params.id};
   Poll.findOneAndRemove(query).then(function(poll){
     res.send(poll);
